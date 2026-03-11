c17d61d4162296e847fb27a23421c81c86f14d67
moussa Traore <moutra213@gmail.com>
2026-03-11 01:18:07 +0100

    la fin de la mise en place des pages principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import CheckoutPage from './composants/checkoutPage'
 import HomePage from './composants/HomePage'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import {Routes,Route } from 'react-router-dom'
+import OrderPage from './composants/OrderPage'
+import TrackingPage from './composants/TrackingPage'
 
 function App() {
 
   return (
     <>
-        <BrowserRouter>
               <Routes>
-                      <Route path='/' element={<HomePage/>}/>
+                      <Route index element={<HomePage/>}/>
+                      <Route path='checkout' element={<CheckoutPage/>}/>
+                      <Route path='order' element={<OrderPage/>}/>
+                      <Route path='tracking' element={<TrackingPage/>}/>
+
+
               </Routes>
-        </BrowserRouter>
     </>
   )
 }
